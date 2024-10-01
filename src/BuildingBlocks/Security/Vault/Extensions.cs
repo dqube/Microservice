667be9c3cbe60d7ae038c3408dbc0788d3bd9a4c
Dev <mdevendran@outlook.com>
2024-10-01 17:40:19 -0300

    vv imp

--- src/BuildingBlocks/Security/Vault/Extensions.cs
+++ src/BuildingBlocks/Security/Vault/Extensions.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CompanyName.MyProjectName.BuildingBlocks.Security.Vault;
 
@@ -13,9 +15,66 @@ public static class Extensions
         var section = builder.Configuration.GetSection(SectionName);
         var vaultOptions = new VaultOptions();
         section.Bind(vaultOptions);
+        string env = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")) ? "Development" : Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Unknown";
+        RuntimeSettings runtime = RuntimeSettings.Create(env);
+        runtime.UAMI = builder.Configuration.GetSection("UserAssignedManagedIdentityClientId").Value ?? "Unknown";
+        builder.Services.AddSingleton<RuntimeSettings>(runtime);
         if (vaultOptions.Enabled)
         {
-            builder.Configuration.AddAzureAppConfiguration(vaultOptions.Endpoint);
+            builder.Configuration.AddAzureAppConfiguration(
+                opts =>
+#pragma warning disable SA1117 // Parameters should be on same line or separate lines
+            {
+                switch (runtime.CurrentEnvironment)
+                {
+                    case RuntimeSettings.Environment.Development:
+                        var tid = builder.Configuration.GetSection("AppConfig:ServicePrincipal:TenantId").Value ?? "3ecb6e23-e454-47b7-8259-162345d74e10";
+                        var cid = builder.Configuration.GetSection("AppConfig:ServicePrincipal:ClientId").Value ?? "0d07909a-542b-46ff-8c77-f619a90d6a92";
+                        var sec = builder.Configuration.GetSection("AppConfig:ServicePrincipal:Secret").Value ?? "hr98Q~y6wC42jM82QyI7HX1h1b0QeU8MHgJkIcrC";
+                        opts.Connect(vaultOptions.Endpoint).ConfigureKeyVault(kv =>
+                        {
+                            kv.SetCredential(new ClientSecretCredential(tid, cid, sec));
+                            kv.SetSecretRefreshInterval("API:Settings:Secrets", TimeSpan.FromHours(24));
+                        });
+
+                        break;
+                    case RuntimeSettings.Environment.Production:
+                        string c = builder.Configuration.GetSection("AppConfiguration").Value ?? throw new Exception("AppConfiguration not set");
+
+                        opts.Connect(new Uri(c), new ManagedIdentityCredential(runtime.UAMI)).ConfigureKeyVault(kv =>
+                        {
+                            kv.SetCredential(new ManagedIdentityCredential(runtime.UAMI));
+                            kv.SetSecretRefreshInterval("API:Settings:Secrets", TimeSpan.FromHours(24));
+                        });
+                        break;
+                    case RuntimeSettings.Environment.Test or RuntimeSettings.Environment.PPE:
+                        throw new ArgumentException("Confiuration needed");
+                    default:
+                        throw new ArgumentException("Environment not set");
+                }
+
+                opts
+                          .Select("Settings:*")
+                          .Select("API:*")
+                          .Select($"API:{env}")
+                          .ConfigureRefresh(refresh =>
+                          {
+                              refresh
+                                .Register("Settings", refreshAll: true)
+                                .SetCacheExpiration(TimeSpan.FromDays(1));
+                          });
+
+                          // .UseFeatureFlags(featureFlagOptions =>
+                          // {
+                          //    featureFlagOptions.CacheExpirationInterval = TimeSpan.FromDays(1);
+                          //    featureFlagOptions.Select(KeyFilter.Any, LabelFilter.Null).Select(KeyFilter.Any, env);
+                          // });
+
+                // builder.Services.AddSingleton<IConfigurationRefresher>(opts.GetRefresher());
+            }, optional: true);
+#pragma warning restore SA1117 // Parameters should be on same line or separate lines
+
+            // builder.Configuration.AddAzureAppConfiguration(vaultOptions.Endpoint);
 
             // builder.Configuration.AddAzureAppConfiguration(options =>
             // {
---
+++ src/BuildingBlocks/Security/Vault/RuntimeSettings.cs
@@ -0,0 +1,42 @@
+﻿namespace CompanyName.MyProjectName.BuildingBlocks.Security.Vault;
+
+    public class RuntimeSettings
+    {
+        public static RuntimeSettings Create(string env)
+        {
+            Enum.TryParse(env, out Environment environment);
+            var v = new RuntimeSettings();
+            v.CurrentEnvironment = environment;
+            return v;
+        }
+
+        /// <summary>
+        /// Specifies the environment in which the application is running.
+        /// </summary>
+        public enum Environment
+        {
+            /// <summary>
+            /// The development environment.
+            /// </summary>
+            Development,
+
+            /// <summary>
+            /// The production environment.
+            /// </summary>
+            Production,
+
+            /// <summary>
+            /// The test environment.
+            /// </summary>
+            Test,
+
+            /// <summary>
+            /// The pre-production environment.
+            /// </summary>
+            PPE
+        }
+
+        public Environment CurrentEnvironment { get; set; }
+
+        public string UAMI { get; set; } = string.Empty;
+}
